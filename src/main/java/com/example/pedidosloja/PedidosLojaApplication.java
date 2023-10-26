53087fbcfa421a53cb3872d51c0863c10d457ede
Gabriel Resende <gabriellrr2005@gmail.com>
2023-10-26 16:11:16 -0300

    project created

---
+++ src/main/java/com/example/pedidosloja/PedidosLojaApplication.java
@@ -0,0 +1,13 @@
+package com.example.pedidosloja;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PedidosLojaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PedidosLojaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/pedidosloja/PedidosLojaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pedidosloja;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PedidosLojaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
