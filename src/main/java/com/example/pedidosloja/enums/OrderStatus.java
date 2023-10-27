2e1d67f2aed06e06a70e4809cb242bba15cc9a38
Gabriel Resende <gabriellrr2005@gmail.com>
2023-10-27 14:44:51 -0300

    OrderStatus added

---
+++ src/main/java/com/example/pedidosloja/enums/OrderStatus.java
@@ -0,0 +1,24 @@
+package com.example.pedidosloja.enums;
+
+public enum OrderStatus {
+    WAITING_PAYMENT(1), PAID(2), DELIVERED(3), CANCELED(4);
+
+    private int code;
+
+    OrderStatus(int code) {
+        this.code = code;
+    }
+
+    public int getCode() {
+        return code;
+    }
+
+    public static OrderStatus valueOf(int code) {
+        for (OrderStatus value : OrderStatus.values()) {
+            if (value.getCode() == code) {
+                return value;
+            }
+        }
+        throw new IllegalArgumentException("Invalid OrderStatus code");
+    }
+}
--- src/main/java/com/example/pedidosloja/model/Order.java
+++ src/main/java/com/example/pedidosloja/model/Order.java
@@ -1,32 +1,49 @@
 package com.example.pedidosloja.model;
 
+import com.example.pedidosloja.enums.OrderStatus;
 import jakarta.persistence.*;
-import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
+@EqualsAndHashCode(of = "id")
+@NoArgsConstructor
+@Getter
 @Entity
 @Table(name = "orders")
-@Data
 public class Order {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    private LocalDateTime date = LocalDateTime.now();
+    private LocalDateTime date;
 
     @Column(nullable = false)
-    private BigDecimal totalValue = BigDecimal.ZERO;
+    private BigDecimal totalValue;
 
     @ManyToOne(fetch = FetchType.LAZY)
     private Client client;
 
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
+    private OrderStatus status;
+
     @OneToMany(mappedBy = "order", cascade = CascadeType.ALL)
-    private List<OrderItem> orderItems = new ArrayList<>();
+    private List<OrderItem> orderItems;
+
+    public Order(Client client) {
+        this.client = client;
+        date = LocalDateTime.now();
+        totalValue = BigDecimal.ZERO;
+        status = OrderStatus.WAITING_PAYMENT;
+        orderItems = new ArrayList<>();
+    }
 
     public void addItem(OrderItem item) {
         item.setOrder(this);
